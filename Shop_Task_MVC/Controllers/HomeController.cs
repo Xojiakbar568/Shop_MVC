f9241b7be1ddbd9dfc58f4aa5af6b0c4192b009e
108 <108@DESKTOP-HVT0I5F>
2023-06-21 16:28:02 +0500

    Add project files.

---
+++ Shop_Task_MVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Task_MVC.Models;
+using System.Diagnostics;
+
+namespace Shop_Task_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Shop> shop = new List<Shop>
+        {
+            new Shop
+            {
+                Id = 1,
+                Name = "Olma",
+                Price = 14000
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(shop);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(shop);
+        }
+        [HttpPost]
+        public IActionResult Add(Shop shoping)
+        {
+            return View(shop);
+        }
+
+
+    }
+}
---
+++ Shop_Task_MVC/Models/Shop.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop_Task_MVC.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
